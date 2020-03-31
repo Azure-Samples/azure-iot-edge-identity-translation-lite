1d77b79944d958563ce02da883fb20a4d0970ec6
Michiel van Schaik <mivansch@microsoft.com>
2020-03-31 09:19:28 +0200

    Added license information to source code files of edge modules.

--- src/edge/modules/ptm-mqtt/app/libs/__init__.py
+++ src/edge/modules/ptm-mqtt/app/libs/__init__.py
@@ -1,2 +1,5 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 __version__ = '0.1'
 VERSION=__version__
